cdc32cdb366a47b187ec569cdfa33be1a6f8de47
mathias-villena <mathias.villena@tecsup.edu.pe>
2026-06-28 18:11:20 -0500

    fix: add vercel.json rewrite rule for SPA routing

--- stockella_mobile/lib/core/constants/api_constants.dart
+++ stockella_mobile/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.100.100:4000/api";// cambiar x la ip de tu laptop a tu wifi
-  static const String mlUrl = "http://192.168.100.100:8000"; // cambiar x la ip de tu laptop a tu wifi
+  static const String baseUrl = "https://stockella-backend.onrender.com/api";
+  static const String mlUrl = "https://stockella-ml.onrender.com";
 }
 
